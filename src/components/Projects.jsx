a6a25472be0d61ce0e74fe22afa699771d51e36f
AbdulSamad <ansamad1028@gmail.com>
2024-07-07 19:31:39 +0530

    Removed unnecessary variables

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,13 +4,11 @@ import ProjectItem from './ProjectItem'
 const Projects = () => {
 	const attendanceMonitoringDesc = 'A Blockchain Enabled Attendance Monitoring system that uses face recognition to mark attendance.';
 	const fakeNewsDesc = 'Fake News Detection system utilizing LSTM neural network to identify and stop the spread of false news.';
-	const healthVaultDesc = 'A Blockchain based health records management system that gives complete control of a patients medical record to the patient.';
 	const intrusionDetectionDesc = 'IoT enabled Intrusion Detection System that captures pictures of the intruder and sends them to the users telegram using a telegram bot.';
 	const foodBuddyDesc = 'React application to discover a vast variety of dishes and how to make them'
 
 	const attendanceMonitoringLink = 'https://github.com/Sam-822/Attendance-Monitoring-System';
 	const fakeNewsLink = 'https://github.com/Sam-822/FAKE-NEWS-DETECTION';
-	const healthVaultLink = '';
 	const intrusionDetectionLink = 'https://github.com/Sam-822/Intrusion-Detection-System';
 	const foodBuddyLink = 'https://foodbuddyabdul.netlify.app/'
 	return (
