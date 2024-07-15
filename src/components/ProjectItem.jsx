d2e667c4a437d88e9ef5f89e6dc8e2902b651f35
AbdulSamad <ansamad1028@gmail.com>
2024-07-15 17:07:02 +0530

    GlassMorphism project items

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,40 +1,52 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import attendance from '../assets/AttendanceMonitoring.png'
-import fakeNews from '../assets/FakeNews.png'
-import healthVault from '../assets/HealthVault.png'
-import intrusion from '../assets/IntrusionDetection.png'
-import foodBuddy from '../assets/FoodBuddy.png'
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import attendance from "../assets/AttendanceMonitoring.png";
+import fakeNews from "../assets/FakeNews.png";
+import healthVault from "../assets/HealthVault.png";
+import intrusion from "../assets/IntrusionDetection.png";
+import foodBuddy from "../assets/FoodBuddy.png";
 
 const ProjectItem = (props) => {
-	const { img, title, description, link, buttonText} = props;
-	let image = ''
-  if(img === 'AttendanceMonitoring.png'){
-		image = attendance
-	}
-	else if(img === 'FakeNews.png'){
-		image = fakeNews
-	}
-	else if(img === 'HealthVault.png'){
-		image = healthVault
-	}else if(img ==='IntrusionDetection.png'){
-		image = intrusion
-	}else if(img ==='FoodBuddy.png'){
-		image = foodBuddy
-	}
-	return (
-		<Card id='project-item-card' className='mx-2 my-4 autoShow' style={{ width: '32rem', height:'fit-content' }}>
+  const { img, title, description, link, buttonText } = props;
+  let image = "";
+  if (img === "AttendanceMonitoring.png") {
+    image = attendance;
+  } else if (img === "FakeNews.png") {
+    image = fakeNews;
+  } else if (img === "HealthVault.png") {
+    image = healthVault;
+  } else if (img === "IntrusionDetection.png") {
+    image = intrusion;
+  } else if (img === "FoodBuddy.png") {
+    image = foodBuddy;
+  }
+  return (
+    <Card
+      id="project-item-card"
+      className="glass mx-2 my-4 autoShow"
+      style={{
+        width: "32rem",
+        height: "fit-content"
+      }}
+    >
       <Card.Img variant="top" src={image} />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
-        <Card.Text>
-          {description}
-        </Card.Text>
-        <Button variant="primary" href={link} target='_blank' className='stretched-link'>{buttonText==='deploy'? 'View Deployed Project': 'View Project on Github'}</Button>
+        <Card.Text>{description}</Card.Text>
+        <Button
+          variant="primary"
+          href={link}
+          target="_blank"
+          className="stretched-link"
+        >
+          {buttonText === "deploy"
+            ? "View Deployed Project"
+            : "View Project on Github"}
+        </Button>
       </Card.Body>
     </Card>
-	)
-}
+  );
+};
 
-export default ProjectItem
+export default ProjectItem;
