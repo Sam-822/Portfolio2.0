332bed5da26f47632b9583f756858aaf753c2285
AbdulSamad <ansamad1028@gmail.com>
2024-07-30 16:10:40 +0530

    changed title

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,26 +13,7 @@ const NavBar = () => {
     >
       <Container>
         <Navbar.Brand href="/">
-          <svg
-					id="brand-logo"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlSpace="preserve"
-            height="40px"
-            version="1.1"
-						style={{shapeRendering:'geometricPrecision', textRendering:'geometricPrecision', imageRendering:'optimize-Quality', fillRule:'evenodd', clipRule:'evenodd'}}
-            viewBox="0 0 1931.65 2923.88"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-            xmlns:xodm="http://www.corel.com/coreldraw/odm/2003"
-          >
-            <g id="Layer_x0020_1">
-              <metadata id="CorelCorpID_0Corel-Layer" />
-              <path
-                id="logo"
-                className="fil0 str0"
-                d="M865.71 1132.97l0 -200.25 200.24 0 0 200.25 772.36 0c0,0 -100.12,-0 -100.12,-100.12 0,-100.12 100.12,-100.12 100.12,-100.12l-772.36 -858.18 0 286.06c0,0 -0.01,100.12 -100.12,100.12 -100.12,0 -100.12,-86.93 -100.12,-86.93l0 -156.23 -772.37 715.15c0,0 100.12,-0 100.12,100.12 0,100.12 -100.12,100.12 -100.12,100.12l972.61 657.94 0 200.25 -200.24 0 0 -200.25 -772.37 0c0,0 100.12,0 100.12,100.12 0,100.12 -100.12,100.12 -100.12,100.12l772.37 858.18 0 -286.06c0,0 -0.01,-100.12 100.12,-100.12 100.12,0 100.12,100.12 100.12,100.12l0 143.03 772.36 -715.15c0,0 -100.12,0 -100.12,-100.12 0,-100.12 100.12,-100.12 100.12,-100.12l-972.6 -657.94z"
-              />
-            </g>
-          </svg>
+				<img src="/logo.svg" height={40} width={40} alt="" />
         </Navbar.Brand>
         <Navbar.Brand id="brand-name" href="/" className="text-white fs-3 fw-semibold" style={{marginLeft:'30px'}}>Abdul Samad Ansari</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
